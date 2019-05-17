9d90fb9fa544c58ff1fbf624e02f1e5f8a33306f
Johannes Strauß <jstrauss@evoila.de>
2019-05-17 11:52:50 +0200

    feat(): more/less search
    add option alter the display of packages

--- cmd/Search.go
+++ cmd/Search.go
@@ -8,32 +8,45 @@ import (
 )
 import . "github.com/evoila/BPM-Client/model"
 
-func SearchByPublisher(publisher string, config *Config, openId *gocloak.JWT) {
+func SearchByPublisher(publisher string, config *Config, openId *gocloak.JWT, more bool) {
+
 	body, statusCode := rest.GetMetaDataListByPublisher(config, openId, publisher)
 
 	if statusCode == 200 {
-		var metaData = body.Embedded.Packages
-
-		for _, d := range metaData {
-			fmt.Println(d.String2())
-			fmt.Println("├─")
+		if more {
+			printResultMore(body.Embedded.Packages)
+		} else {
+			printResultLess(body.Embedded.Packages)
 		}
 	} else {
 		fmt.Println("No Packages Found. Status: " + strconv.Itoa(statusCode))
 	}
 }
 
-func SearchByPublisherAndName(publisher, name string, config *Config, openId *gocloak.JWT) {
+func SearchByPublisherAndName(publisher, name string, config *Config, openId *gocloak.JWT, more bool) {
 	body, statusCode := rest.GetMetaDataListByPublisherAndName(config, openId, publisher, name)
 
 	if statusCode == 200 {
-		var metaData = body.Embedded.Packages
-
-		for _, d := range metaData {
-			fmt.Println(d.String2())
-			fmt.Println("├─")
+		if more {
+			printResultMore(body.Embedded.Packages)
+		} else {
+			printResultLess(body.Embedded.Packages)
 		}
 	} else {
 		fmt.Println("No Packages Found. Status: " + strconv.Itoa(statusCode))
+	}
+}
+
+func printResultLess(metaData []MetaData) {
+	for _, d := range metaData {
+		fmt.Println("├─" + d.Publisher + ":" + d.Name + ":" + d.Version)
+	}
+}
+
+func printResultMore(metaData []MetaData) {
+
+	for _, d := range metaData {
+		fmt.Println(d.String2())
+		fmt.Println("├─")
 	}
 }
